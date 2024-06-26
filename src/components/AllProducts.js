e7afd2cd378adfd7f5ca9a30116a93d1c8ed0a74
mushfiq <mushfiqxrabbi@gmail.com>
2024-06-26 20:30:01 +0600

    ...

--- src/components/AllProducts.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import SingleProduct from "./SingleProduct";
-
-export default function AllProducts({ products }) {
-  return products.map((product) => {
-    return <SingleProduct key={product.id} product={product} />;
-  });
-}
